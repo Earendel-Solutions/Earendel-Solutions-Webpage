d31feff2c2cea6bea2c05afd0f2307b1c76b2d9c
Farhan Mir <work.farhanmir@gmail.com>
2026-06-06 21:52:12 +0530

    vercel change test

--- my-app/app/_components/home.tsx
+++ my-app/app/_components/home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-blue-900 to-black">
       <Navbar />
       <div className="text-3xl font-medium animate-bounce underline bg-gradient-to-r from-blue-400 to-white bg-clip-text text-transparent">
-        Home Page
+        vercel change test
       </div>
     </div>
   );
