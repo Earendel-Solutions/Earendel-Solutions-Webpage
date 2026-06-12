8dc6e06f6525daf95e2ac2886d3d9e0e55acf7b8
sheikkaheel <sheikhkaheel@gmail.com>
2026-06-12 21:42:00 +0530

    main change

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,7 +3,7 @@ export default async function Page() {
     <div className="w-full overflow-x-hidden">
       <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-blue-900 to-black">
         <div className="text-3xl font-medium animate-bounce underline bg-gradient-to-r from-blue-400 to-white bg-clip-text text-transparent">
-          vercel change test
+          vercel change
         </div>
       </div>
     </div>
