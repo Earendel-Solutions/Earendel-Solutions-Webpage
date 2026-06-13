e192cff4bfaf9cddff02921cb8b613ab009dfab3
abuArsalan <arsalanshafi911@gmail.com>
2026-06-13 14:55:57 +0530

    cards are responsive now

--- my-app/app/project/_components/card.tsx
+++ my-app/app/project/_components/card.tsx
@@ -7,10 +7,10 @@ type CardProps  =  {
 
 export default function Card({name = "default",des="default"}:CardProps){
     return(
-	<div className=" flex flex-col items-start px-[70px] py-[15px] m-[40px] color bg-gradient-to-r from-cyan-900 to-black text-white shadow-black shadow-2xl basis-[40%] rounded-lg">	
-	<div className="outline-[2px] border-white h-[300px] w-[100%]">image goes here</div>
-	<div className="text-white text-[40px]">{name}</div>
-	<div className="text-white">{des}</div>
+	<div className=" flex flex-col items-start px-[70px] py-[15px] m-[40px] color bg-gradient-to-r from-cyan-900 to-black text-white shadow-black shadow-2xl basis-[60%] rounded-lg lg:basis-[40%]">	
+		<div className="outline-[2px] border-white h-[150px] w-[100%] lg:h-[200px]">image goes here</div>
+		<div className="text-white text-[20px] lg:text-[40px]">{name}</div>
+		<div className="text-white text-[8px] lg:text-[15px]">{des}</div>
 	</div>
     );
 
--- my-app/app/project/page.tsx
+++ my-app/app/project/page.tsx
@@ -2,11 +2,10 @@ import Card from "./_components/card"
 export default function Projects() {
   return (
 
-    <div className="w-[100%]  bg-gradient-to-r from-cyan-700 to-black pt-[100px]">
-      <div className="flex border-red outline-[2px] w-[100%] flex items-center justify-center flex-wrap">
+    <div className="w-[100%]   bg-black pt-[100px]">
+      <div className="flex w-[100%]  flex items-center justify-center flex-wrap">
         <Card name = "Full Stack" des = "Check out our full stack projects"/>
 	<Card name = "Android" des = " check out our android projects"/>
-	<Card/>
 	<Card/>
 	<Card/>
       </div>
