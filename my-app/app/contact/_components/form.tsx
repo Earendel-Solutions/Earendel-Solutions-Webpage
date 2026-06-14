0d3e9ff0116cbee5738455c9eb17e5b83d7c7600
Farhan Mir <work.farhanmir@gmail.com>
2026-06-14 10:13:24 +0530

    Added react form hook, implimented form using form hook.

--- my-app/app/contact/_components/form.tsx
+++ my-app/app/contact/_components/form.tsx
@@ -1,66 +1,90 @@
 "use client";
 
-export default function ContactForm(){
-    return(
-        <div className="border border-white rounded-3xl min-h-[550px] p-8  text-white">
-            <div className="space-y-3 mb-8">
-              <h3 className="text-2xl font-bold text-white">
-                Project Consultation
-              </h3>
+import { useForm } from "react-hook-form";
 
-              <p className="text-[#c2c6d6]">
-                Tell us about your project and we'll get back to you within 24
-                hours.
-              </p>
-            </div>
+type FormType = {
+  name: string;
+  email: string;
+  service: string;
+  description: string;
+};
 
-            <form className="space-y-5">
-              <input
-                type="text"
-                placeholder="Your Name"
-                className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white placeholder:text-gray-400 outline-none focus:border-purple-500"
-              />
+export default function ContactForm() {
 
-              <input
-                type="email"
-                placeholder="Email Address"
-                className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white placeholder:text-gray-400 outline-none focus:border-purple-500"
-              />
+      const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormType>();
 
-              <select
-                className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white outline-none focus:border-purple-500"
-                defaultValue=""
-              >
-                <option value="" disabled className="bg-black/90 text-white">
-                  Select Service
-                </option>
+   const onSubmit = async (data: FormType) => {
+    console.log("Form Data -> ", data);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      alert("Form sent");
 
-                <option className="bg-black/90 text-white">UI/UX Design</option>
-                <option className="bg-black/90 text-white">AI Solutions</option>
-                <option className="bg-black/90 text-white">Automation</option>
-                <option className="bg-black/90 text-white">Consultation</option>
-                <option className="bg-black/90 text-white">
-                  Web Application
-                </option>
-                <option className="bg-black/90 text-white">Other</option>
-                <option className="bg-black/90 text-white">
-                  Website Development
-                </option>
-              </select>
+  };
 
-              <textarea
-                rows={6}
-                placeholder="Describe your project, goals, requirements, or challenges..."
-                className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white placeholder:text-gray-400 outline-none resize-none focus:border-purple-500"
-              />
+  return (
+    <div className="border border-white rounded-3xl min-h-[550px] p-8  text-white">
+      <div className="space-y-3 mb-8">
+        <h3 className="text-2xl font-bold text-white">Project Consultation</h3>
 
-              <button
-                type="submit"
-                className="w-full rounded-xl bg-purple-600 py-3 font-semibold text-white transition-all hover:bg-purple-500"
-              >
-                Book a Free Consultation
-              </button>
-            </form>
-          </div>
-    )
+        <p className="text-[#c2c6d6]">
+          Tell us about your project and we'll get back to you within 24 hours.
+        </p>
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <input
+          type="text"
+          placeholder="Your Name"
+          {...register("name")}
+          className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white placeholder:text-gray-400 outline-none focus:border-purple-500"
+        />
+
+        <input
+          type="email"
+          placeholder="Email Address"
+          {...register("email")}
+          className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white placeholder:text-gray-400 outline-none focus:border-purple-500"
+        />
+
+        <select
+        {...register("service")}
+          className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white outline-none focus:border-purple-500"
+          
+        >
+          <option value="" disabled className="bg-black/90 text-white">
+            Select Service
+          </option>
+
+          <option className="bg-black/90 text-white">UI/UX Design</option>
+          <option className="bg-black/90 text-white">AI Solutions</option>
+          <option className="bg-black/90 text-white">Automation</option>
+          <option className="bg-black/90 text-white">Consultation</option>
+          <option className="bg-black/90 text-white">Web Application</option>
+          <option className="bg-black/90 text-white">Other</option>
+          <option className="bg-black/90 text-white">
+            Website Development
+          </option>
+        </select>
+
+        <textarea
+        {...register("description")}
+          rows={6}
+          placeholder="Describe your project, goals, requirements, or challenges..."
+          className="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white placeholder:text-gray-400 outline-none resize-none focus:border-purple-500"
+        />
+
+        <button
+          type="submit"
+          disabled = {isSubmitting}
+          className="w-full rounded-xl bg-purple-600 py-3 font-semibold text-white transition-all hover:bg-purple-500"
+        >
+            {isSubmitting ? "Sending..." : "Book a Free Consultation"}
+          
+        </button>
+      </form>
+    </div>
+  );
 }
