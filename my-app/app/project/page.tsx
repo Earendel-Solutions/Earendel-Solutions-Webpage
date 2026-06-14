d9dcb483679fd29e79a5dd14a9a7570de0d157b6
abuArsalan <arsalanshafi911@gmail.com>
2026-06-14 11:23:17 +0530

    doing it again because someone decided to make the repo private

--- my-app/app/project/page.tsx
+++ my-app/app/project/page.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
 	</div>
       	<div className="flex w-[100%]  flex items-center justify-center flex-wrap">
 		<Card name = "Full Stack" des = "Check out our full stack projects"/>
-		<Card name = "Android" des = " check out our android projects"/>
+		<Card name = "Android" des = " Check out our android projects"/>
 		<Card/>
 		<Card/>
       	</div>
